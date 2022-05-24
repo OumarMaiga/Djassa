56a09dcd0b03c7e7a3a4d85dd3f74c416d9d06e9
Daniel Tossou <tossdaniel@gmail.com>
2022-05-24 08:58:12 +0000

    Changed link to product details Added breadcumb

--- resources/views/dashboards/products/show.blade.php
+++ resources/views/dashboards/products/show.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
-    <div class="container" style="margin-top: 10rem">
+    <div class="container" style="margin-top: 7rem">
+    <p style="font-size:18px; font-weight:500">Djassa > Fruits & Légumes > Légumes à racine > Pommes de terre</p>
         <div class="row" style="margin-top:3%; margin-bottom:5%">
             <div class="col-7" style="display:flex; justify-content:center">
                 <img src="{{ asset('images/pomme-de-terre.webp') }}" alt="">
@@ -22,16 +23,16 @@
 
         <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true" style="font-weight:600; font-size:20px">Principales caractéristiques</button>
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true" style="font-weight:600; font-size:20px; color:#1A1A1A">Principales caractéristiques</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false" style="font-weight:600;font-size:20px">Conservation et utilisation</button>
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false" style="font-weight:600;font-size:20px; color:#1A1A1A">Conservation et utilisation</button>
             </li>
             <li class="nav-item" role="presentation">
-                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false" style="font-weight:600;font-size:20px">Autres indications</button>
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false" style="font-weight:600;font-size:20px; color:#1A1A1A">Autres indications</button>
             </li>
         </ul>
-        <div class="tab-content" id="myTabContent">
+        <div class="tab-content" id="myTabContent" style="margin-bottom:2rem">
             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab" style="margin-top:3%"> 
                 <h3 style="font-weight:700;font-size:18px">Marque & labels <span style="font-weight:400;font-size:17PX; margin-left:6.5%">BELLE FRANCE</span></h3>
                 <h3 style="font-weight:700;font-size:18px; margin-top:2rem">Propriétés <span style="font-weight:400;font-size:17PX; margin-left:10.4%">Bio</span></h3>
@@ -44,14 +45,14 @@
 
             <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab" style="margin-top:3%"> 
                 <p>
-                <span style="font-weight:700;font-size:18px">Conservation</span><span style="font-weight:400;font-size:17PX; margin-left:6.5%">Conserver les pommes de terre à l'abri de la lumière et ailleurs qu'au réfrigérateur. Température conseillée: 8 à 10 °C environ.</span>
+                <span style="font-weight:700;font-size:18px">Conservation</span><span style="font-weight:400;font-size:17PX; margin-left:9%">Conserver les pommes de terre à l'abri de la lumière et ailleurs qu'au réfrigérateur. Température conseillée: 8 à 10 °C environ.</span>
                 </p>
                 <p style="margin-top:2rem">
-                <span style="font-weight:700;font-size:18px; margin-top:2rem">Renseignements<br> pratiques</span><span style="font-weight:400;font-size:17PX; margin-left:10.4%">Idéales pour poêlée et aussi confites à la provençale</span>
+                <span style="font-weight:700;font-size:18px; margin-top:2rem">Renseignements<br> pratiques</span><span style="font-weight:400;font-size:17PX; margin-left:11.4%">Idéales pour poêlée et aussi confites à la provençale</span>
                 </p> 
                 <p style="margin-top:2rem">
                 <span style="font-weight:700;font-size:18px; margin-top:2rem">Complément d'info</span><span style="font-weight:400;font-size:17PX; margin-left:4.7%">
-                    Plutôt petite, mais pleine de goût, cette variété se présente sous une forme fine et délicate qui dissimule une chair étonnamment ferme. Elle s’accorde avec les raclettes, les plateaux de fromages et donne de savoureuses salades.</span>
+                    Plutôt petite, mais pleine de goût, cette variété se présente sous une forme fine et délicate qui dissimule une chair étonnamment ferme. <br> <span style="margin-left:14rem">Elle s’accorde avec les raclettes, les plateaux de fromages et donne de savoureuses salades.</span></span>
                 </p>
                 
             </div>
