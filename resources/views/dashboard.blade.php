3a30b5b6aaa5d6b0b170a1409d4f1fb236c0c6e4
Daniel Tossou <tossdaniel@gmail.com>
2022-06-28 19:21:53 +0000

    Modified informations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,9 @@
 
     <div class="container" style="margin-top:6.5rem">
         <div class="row">
+        
             <div class="col-4 mt-3">
+            <a href="{{ route('dashboard.product.index') }}">
                 <div class="card shadow-sm">
                     <div class="card-body">
                         <center>
@@ -13,8 +15,11 @@
                         </center>
                     </div>
                 </div>
+                </a>
             </div>
+        
             <div class="col-4 mt-3">
+            <a href="http://127.0.0.1:8000/dashboard/recettes">
                 <div class="card shadow-sm">
                     <div class="card-body">
                         <center>
@@ -25,18 +30,35 @@
                         </center>
                     </div>
                 </div>
+            </a>
             </div>
             <div class="col-4 mt-3">
-                <div class="card shadow-sm">
-                    <div class="card-body">
-                        <center>
-                            <div class="mt-2"><ion-icon name="people-outline" style="font-size:36px"></ion-icon></div>
-                            <h2 class="mb-2" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px"">
-                                UTILISATEURS
-                            </h2>
-                        </center>
+                <a href="{{ route('dashboard.user.index') }}">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <center>
+                                <div class="mt-2"><ion-icon name="people-outline" style="font-size:36px"></ion-icon></div>
+                                <h2 class="mb-2" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px"">
+                                    UTILISATEURS
+                                </h2>
+                            </center>
+                        </div>
                     </div>
-                </div>
+                </a>
+            </div>
+            <div class="col-4 mt-3">
+                <a href="{{ route('dashboard.rayon.index') }}">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <center>
+                                <div class="mt-2"><ion-icon name="file-tray-stacked-outline" style="font-size:36px"></ion-icon></div>
+                                <h2 class="mb-2" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px"">
+                                    RAYONS
+                                </h2>
+                            </center>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
--- resources/views/dashboards/rayons/index.blade.php
+++ resources/views/dashboards/rayons/index.blade.php
@@ -1,10 +1,55 @@
 <x-app-layout>
     <div class="" style="margin-top: 6rem">
         <div class="container">
-            <h3 class="mb-3" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px"">
+            <div class="row">
+                <div class="col-4 mt-3">
+                    <a href="{{ route('dashboard.category.index') }}">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <center>
+                                    <div class="mt-2"><ion-icon name="file-tray-stacked-outline" style="font-size:32px"></ion-icon></div>
+                                    <h2 class="mb-2" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:16px"">
+                                        CATÉGORIE
+                                    </h2>
+                                </center>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-4 mt-3">
+                    <a href="{{ route('dashboard.sub_category.index') }}">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <center>
+                                    <div class="mt-2"><ion-icon name="file-tray-stacked-outline" style="font-size:32px"></ion-icon></div>
+                                    <h2 class="mb-2" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:16px"">
+                                        SOUS-CATÉGORIE
+                                    </h2>
+                                </center>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-4 mt-3">
+                    <a href="{{ route('dashboard.sub_sub_category.index') }}">
+                        <div class="card shadow-sm">
+                            <div class="card-body">
+                                <center>
+                                    <div class="mt-2"><ion-icon name="file-tray-stacked-outline" style="font-size:32px"></ion-icon></div>
+                                    <h2 class="mb-2" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:16px"">
+                                        SOUS SOUS-CATÉGORIE
+                                    </h2>
+                                </center>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <span class="mr-2" style="float: left; display: inline-block; padding-top:0.5rem; cursor: pointer;" id="go-back"><ion-icon name="return-up-back-outline" style="font-size:36px;"></ion-icon></span>
+            <h3 class="mb-3 mt-6" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px"">
                     LES RAYONS
             </h3>
-            <a href="{{ route('dashboard.rayon.create') }}" style="display: inline-block; float: right; padding-top:1rem;"><x-button class="btn-custom">AJOUTER</x-button></a>
+            <a href="{{ route('dashboard.rayon.create') }}" style="display: inline-block; float: right; padding-top:1rem;"><x-button class="btn-custom mt-6">AJOUTER</x-button></a>
 
             <!-- Session Status -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
