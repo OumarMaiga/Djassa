aa265b56e463e411d856d31161a94e67e05cba60
Daniel Tossou <tossdaniel@gmail.com>
2022-08-12 17:23:59 +0000

    Mise à jour de l'affichage détails de service

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -1,76 +1,54 @@
 <x-app-layout>
     <div class="container" style="margin-top: 6rem">
-        <div class="row">
-            <div class="">
-                <span class="underline font-bold">
-                    Titre:
-                </span>
-                <span>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header" style="font-size:20px; font-weight:600;">
                     {{ $service->title }}
-                </span>
+                    <div style="float:right;">
+                        <a href="{{ route('service.edit', $service->id) }}" class="col icon-action icon-edit" style="display:inline-block; margin-right:0.75rem" title="Modifier">
+                            <ion-icon name="create-outline" style="font-size:24px;"></ion-icon>
+                        </a>
+                        <span class="col icon-action" style="display:inline-block">
+                            <form method="POST" action="{{ route('service.destroy', $service->id) }}">
+                                @csrf
+                                @method('delete')
+                                    <button class="" type="submit" onclick="return confirm('Voulez-vous vraiment supprimer ce service?')" title="Supprimer">
+                                        <ion-icon name="trash-outline" style="font-size:24px; color:red;"></ion-icon>
+                                    </button>
+                            </form>
+                        </span>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Special title treatment</h5> -->
+                    <div>
+                        <p class="card-text" style="font-size:18px;display:inline-block;margin-right:2rem;">Etat &nbsp;<span class="btn btn-secondary">{{ $service->etat }}</span></p> 
+                        <p class="card-text" style="font-size:18px;display:inline-block;">Montant:&nbsp;<span style="font-size:18px; font-weight:600;">{{ $service->montant }} FCFA</span></p> 
+                    </div>
+
+                    <div style="margin-top: 2rem">
+                        <p style="font-size:18px; font-weight:600;margin-bottom:1rem;">Bénéficiaire</p>
+                        <div style="display:inline-block;">
+                            <p style="font-size:16px; font-weight:600;margin-bottom:O.5rem;">Nom & Prénom</p>
+                            <p style="display:inline-block;margin-right:2rem;">{{ $service->user }}Damien ursule</p>
+                        </div>
+                        <div style="display:inline-block;">
+                            <p style="font-size:16px; font-weight:600;margin-bottom:O.5rem;">Téléphone</p>
+                            <p style="display:inline-block;margin-right:2rem;">{{ $service->telephone }}</p>
+                        </div>
+                    </div>
+                    
+                    <div style="margin-top: 2rem">
+                        <p style="font-size:18px; font-weight:600;">Description du service</p>
+                        <p>{{ $service->overview }}</p>
+                    </div>
+                </div>
             </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Description:
-                </span>
-                <span>
-                    {{ $service->overview }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Etat:
-                </span>
-                <span>
-                    {{ $service->etat }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Montant:
-                </span>
-                <span>
-                    {{ $service->montant }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Utilisateur:
-                </span>
-                <span>
-                    {{ $service->user }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Email:
-                </span>
-                <span>
-                    {{ $service->email }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Telephone:
-                </span>
-                <span>
-                    {{ $service->telephone }}
-                </span>
-            </div>
+        </div>
+
+        <div class="row">
             <div class="justify-content-between icon-content">
-                <a href="{{ route('service.show', $service->id) }}">Voir</a>
-                <a href="{{ route('service.edit', $service->id) }}" class="col icon-action icon-edit">
-                    Edit
-                </a>
-                <span class="col icon-action">
-                    <form method="POST" action="{{ route('service.destroy', $service->id) }}">
-                        @csrf
-                        @method('delete')
-                            <button class="" type="submit" onclick="return confirm('Vraiment supprimer ce service ?')">
-                                Del
-                            </button>
-                    </form>
-                </span>
+                
                 @if ($service->etat === "request")
                     <a href="{{ route('service.inprogress', $service->id) }}">In proccess</a>
                 @endif
