26ef82e204e246ac350e8a003b9417781c9fdf44
Daniel Tossou <tossdaniel@gmail.com>
2022-08-14 13:43:38 +0000

    Mise à jour détail service des admins

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -22,7 +22,7 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <div style="display:inline-block;margin-bottom:2rem;margin-right:2rem;">
+                        <div style="display:inline-block;margin-bottom:2rem;margin-right:1.5rem;">
                             <p class="" style="display:inline-block;font-weight:600;">Etat: &nbsp;</p><span class="btn btn-secondary" style="display:inline-block;">{{ $service->etat }}</span><br>
                         </div>
                         <div style="display:inline-block;margin-bottom:2rem;">
@@ -49,78 +49,7 @@
                     </div>
                 </div>
             </div>
-
-            <div class="">
-                <span class="underline font-bold">
-                    Titre:
-                </span>
-                <span>
-                    {{ $service->title }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Description:
-                </span>
-                <span>
-                    {{ $service->overview }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Etat:
-                </span>
-                <span>
-                    {{ $service->etat }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Montant:
-                </span>
-                <span>
-                    {{ $service->montant }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Utilisateur:
-                </span>
-                <span>
-                    {{ $service->user }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Email:
-                </span>
-                <span>
-                    {{ $service->email }}
-                </span>
-            </div>
-            <div class="">
-                <span class="underline font-bold">
-                    Telephone:
-                </span>
-                <span>
-                    {{ $service->telephone }}
-                </span>
-            </div>
-            <div class="justify-content-between icon-content">
-                <a href="{{ route('service.show', $service->id) }}">Voir</a>
-                <a href="{{ route('service.edit', $service->id) }}" class="col icon-action icon-edit">
-                    Edit
-                </a>
-                <span class="col icon-action">
-                    <form method="POST" action="{{ route('service.destroy', $service->id) }}">
-                        @csrf
-                        @method('delete')
-                            <button class="" type="submit" onclick="return confirm('Vraiment supprimer ce service ?')">
-                                Del
-                            </button>
-                    </form>
-                </span>
-            </div>
+            
         </div>
         
     </div>
