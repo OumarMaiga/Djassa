d9343e48e06911a1cd8587e6ad82d6cc7d310b60
Daniel Tossou <tossdaniel@gmail.com>
2022-08-17 11:31:50 +0000

    Personnalisation des pages de profil

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,15 +1,17 @@
 <x-app-layout>
     <div class="dashboard-content" style="margin-top: 6rem">
         <div class="container content">
-            <h1 class="content-title" style="margin-bottom:2rem; padding-top:1rem; font-weight:500; font-size:20px">{{ __('REMPLISSEZ LE FORMULAIRE POUR SOUMETTRE UNE DEMANDE') }}</h1>
-    
+            <span class="mr-2" style="float: left; display: inline-block; padding-top:0.5rem; cursor: pointer;" id="go-back"><ion-icon name="return-up-back-outline" style="font-size:36px;"></ion-icon></span>
+        <h3 class="mb-3" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px;">
+                {{ __('MODIFIER MES INFORMATIONS') }}
+            </h3>
             <!-- Session Status -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
     
             <!-- Validation Errors -->
             <x-auth-validation-errors class="mb-4" :errors="$errors" />
     
-            <form method="POST" action="{{ route('user.update', $user->id) }}">
+            <form method="POST" action="{{ route('user.update', $user->id) }}" style="margin-top:1.5rem;">
                 @csrf
                 @method('put')
     
@@ -18,7 +20,7 @@
                     <div class="form-item col-md-6">
                         <!-- Title -->
                         <div>
-                            <x-label for="title" :value="__('Name')" />
+                            <x-label for="title" :value="__('Nom & Prénom')" />
                             <x-input id="title" class="form-control" type="text" name="title"  value="{{ $user->name }}" required autofocus />
                         </div>
                     </div>
@@ -32,7 +34,7 @@
                     <div class="form-item col-md-6">
                         <!-- Montant -->
                         <div>
-                            <x-label for="email" :value="__('Email')" />
+                            <x-label for="email" :value="__('Adresse email')" />
                             <x-input id="email" class="form-control" type="email" name="email"  value="{{ $user->email }}" />
                         </div>
                     </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,30 +1,40 @@
 <x-app-layout>
-    <div class="dashboard-content" style="margin-top: 6rem">
-        <h2>Profile</h2>
+    
+    <div class="container" style="margin-top: 6rem">
+        <span class="mr-2" style="float: left; display: inline-block; padding-top:0.5rem; cursor: pointer;" id="go-back"><ion-icon name="return-up-back-outline" style="font-size:36px;"></ion-icon></span>
+        <h3 class="mb-3" style="display: inline-block; padding-top:1rem; font-weight:500; font-size:20px"">
+            Mon profil
+        </h3>
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <table class="table table-hover table-responsive" style="margin-top: 2rem">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td class="justify-content-between icon-content">
-                        <a href="{{ route('user.edit', $user->id) }}" class="col icon-action icon-edit" style="display:inline-block; margin-right:0.75rem" title="Modifier">
-                            <ion-icon name="create-outline" style="font-size:24px;"></ion-icon>
-                        </a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header" style="font-size:19px; font-weight:600;">
+                        <p style="float:left;">Informations personnelles</p>
+                        <div style="float:right;">
+                            <a href="{{ route('user.edit', $user->id) }}" class="col icon-action icon-edit" style="display:inline-block; margin-right:0.75rem" title="Modifier">
+                                <ion-icon name="create-outline" style="font-size:24px;"></ion-icon>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div style="margin-bottom:2rem;">
+                            <div style="display:inline-block; margin-right:3rem;">
+                                <h4 style="font-weight:600; margin-bottom:0.5rem;">Nom & Prénom</h4>
+                                <p>{{ $user->name }}</p>
+                            </div>
+                            <div style="display:inline-block; margin-right:0.75rem;">
+                                <h4 style="font-weight:600;; margin-bottom:0.5rem;">Email</h4>
+                                <p>{{ $user->email }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 </x-app-layout>
