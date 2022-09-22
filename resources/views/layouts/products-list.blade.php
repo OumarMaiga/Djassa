848f901d067c87a4278b9477dc2521bb20b2b5f5
oumarmaiga <oumarm611@gmail.com>
2022-09-22 02:12:20 +0000

    Accueil responsive

--- resources/views/layouts/products-list.blade.php
+++ resources/views/layouts/products-list.blade.php
@@ -1,13 +1,13 @@
     @foreach ($products as $product) 
-        <div class="col-2 mt-4">
+        <div class="col-md-6 mt-4">
             <div class="card shadow-sm">
-                <a href="{{ route('product.detail', $product->product_slug) }}" class="py-2 mt-2 mx-3" style="background:#F6F6F6;text-align:center;border-radius:4px;font-weight:700; margin-bottom:25%; cursor:pointer">Voir l'offre</a>
-                <img src="{{ asset($product->files_file_path) }}" class="img-responsive mx-3" style="margin-bottom:10%;height:75px;object-fit:cover;" alt="...">
+                <a href="{{ route('product.detail', $product->product_slug) }}" class="py-2 mt-2 mx-3 bg-[#F6F6F6] text-center rounded font-bold mb-[8%] md:mb-[20%] lg:mb-[20%] cursor-pointer">Voir l'offre</a>
+                <img src="{{ asset($product->files_file_path) }}" class="img-responsive mx-3 mb-[4%] md:mb-[10%] lg:mb-[10%] h-44 md:h-20 lg:h-20 object-cover" alt="...">
                 <div class="card-body">
                     @if($product->product_discount != null &&  $product->product_discount > 0)
-                    <p class="px-2" style="background:#ec6333;color:#fff;font-weight:800; font-size:14px; width:40%">-{{ $product->product_discount }}%</p>
+                    <p class="px-2 bg-[#ec6333] text-center md:text-left lg:text-left font-extrabold text-sm text-[#fff] mx-auto w-[20%] md:w-[40%] lg:w-[40%]">-{{ $product->product_discount }}%</p>
                     @endif
-                    <p class="item-offre-title" style="font-size:13px; font-weight:600">{{$product->product_title }}</p>
+                    <p class="item-offre-title text-center md:text-left lg:text-left text-lg md:text-sm lg:text-sm font-semibold">{{$product->product_title }}</p>
                 </div>
             </div>
         </div>
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <!--@include('layouts.sidebar')-->
+    <!---->@include('layouts.sidebar')
 
-    <div class="ml-72 mt-24 md:ml-0.5 sm:ml-0.5" id="container">
+    <div class="ml-1 md:ml-72 mt-24" id="container">
         <!-- Banners -->
         <div class="row" style="margin-right:1rem">
             <div class="col">
@@ -14,7 +14,7 @@
             </div>
         </div>
 
-        <h2 class="text-lg mt-3 font-bold text-black">Les offres de la semaine dans les magasins</h2>
+        <h2 class="text-md md:text-lg lg:text-xl mt-3 font-bold text-black">Les offres de la semaine dans les magasins</h2>
 
         <div id="">
             <div id="products-container" class="row" style="margin-right:1rem">
@@ -29,15 +29,15 @@
         </div>
         <input type="hidden" id="page_number" value="2" />
         <div class="row gx-1" style="margin-top:5%">
-            <div class="col">
+            <div class="col-md">
                 <img src="images/body-lotion.jpg" alt="" style="border-radius:8px">
                 <h2 class="xl:text-xl lg:text-lg md:text-md mt-2 font-bold text-black">Les imbattables de la semaine</h2>
             </div>
-            <div class="col">
+            <div class="col-md">
                 <img src="images/body-lotion.jpg" alt="" style="border-radius:8px">
                 <h2 class="xl:text-xl lg:text-lg md:text-md mt-2 font-bold text-black">Un soin optimal et naturel</h2>
             </div>
-            <div class="col">
+            <div class="col-md">
                 <img src="images/care-products.jpeg" alt="" style="border-radius:8px">
                 <h2 class="xl:text-xl lg:text-lg md:text-md mt-2 font-bold text-black">Du nouveau en beauté</h2>
             </div>
